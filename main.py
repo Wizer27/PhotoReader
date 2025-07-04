b3ba5d2dd06b245e072080344fd807676e5c9013
Wizer27 <svd2yar@gmail.com>
2025-07-04 12:47:49 +0300

    pdf fix

--- main.py
+++ main.py
@@ -17,25 +17,27 @@ a = st.file_uploader("Chose a file")
 if a is not None:
     name = a.name
     print(f"Name: {name}")
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
-        tmp_file.write(a.getvalue())  # Сохраняем содержимое загруженного файла
-        pdf_path = tmp_file.name 
-    try:
+    n = name.split('.')
+    if n[1] == 'pdf':
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
+            tmp_file.write(a.getvalue())  # Сохраняем содержимое загруженного файла
+            pdf_path = tmp_file.name 
+        try:
+                
+            images = convert_from_path(pdf_path,dpi = 300)
             
-        images = convert_from_path(pdf_path,dpi = 300)
-        
-        
-        for i, image in enumerate(images):
-            gray_image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2GRAY)
-            _, threshold_image = cv2.threshold(gray_image, 150, 255, cv2.THRESH_BINARY)
             
-            # 3. Распознаём текст с помощью Tesseract
-            text = pytesseract.image_to_string(threshold_image, lang='rus+eng')  # Языки: русский + английский
-            result_text += f"Страница {i+1}:\n{text}\n\n" 
-        r = st.text_area('Result',value = result_text)
-        st.success('All done !')
-    except FileNotFoundError:    
-        st.error("This file doesnt exist on your computer")    
+            for i, image in enumerate(images):
+                gray_image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2GRAY)
+                _, threshold_image = cv2.threshold(gray_image, 150, 255, cv2.THRESH_BINARY)
+                
+                # 3. Распознаём текст с помощью Tesseract
+                text = pytesseract.image_to_string(threshold_image, lang='rus+eng')  # Языки: русский + английский
+                result_text += f"Страница {i+1}:\n{text}\n\n" 
+            r = st.text_area('Result',value = result_text)
+            st.success('All done !')
+        except FileNotFoundError:    
+            st.error("This file doesnt exist on your computer")    
 
 print(a)
 print(result_text)
